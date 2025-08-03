94eb6662203036b9a82b54855c16aac9dfb5e8a3
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-08-03 14:38:23 +0200

    added inlineStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {OnOff} from "./OnOff/OnOff";
 
 function App() {
   return (
     <div className="App">
-
+      <OnOff on={true}/>
     </div>
   );
 }
---
+++ src/OnOff/OnOff.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+type OnOffTypes = {
+    on: boolean
+}
+
+export const OnOff = ({on}: OnOffTypes) => {
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '5px',
+        backgroundColor: on ? 'green' : 'white',
+    }
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '5px',
+        backgroundColor: on ? 'white' : 'red',
+    }
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '50%',
+        border: '1px solid black',
+        display: 'inline-block',
+        backgroundColor: on ? 'green' : 'red',
+    }
+
+    return (
+        <div className='onOff'>
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    );
+};
+
