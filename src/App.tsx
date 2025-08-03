47e01ae0eae7f34465bbe03a0592e938d7099bc6
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-08-03 15:21:17 +0200

    added mor components OnOff

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <OnOff defaultOn={true}/>
+      <OnOff defaultOn={true}/>
+      <OnOff defaultOn={true}/>
+      <OnOff defaultOn={true}/>
     </div>
   );
 }
